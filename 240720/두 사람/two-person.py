2ddc664ad92aceee3734fd2c95f0824755af7d85
winA <92146550+winnaba61@users.noreply.github.com>
2024-07-20 01:18:37 +0900

    Title: 두 사람, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240720/두 사람/two-person.py
+++ 240720/두 사람/two-person.py
@@ -1,7 +1,7 @@
 age_a,sex_a=map(str,input().split())
 age_b,sex_b=map(str,input().split())
 
-if (int(age_a)>=19 or sex_a=='M') or (int(age_b)>=19 or sex_b=='M'):
+if (int(age_a)>=19 and sex_a=='M') or (int(age_b)>=19 and sex_b=='M'):
     print(1)
 else:
     print(0)
