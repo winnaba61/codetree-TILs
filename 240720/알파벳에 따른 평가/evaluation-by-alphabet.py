595a056a7b1080b34599bd8d4e502e0284f5ce28
winA <92146550+winnaba61@users.noreply.github.com>
2024-07-20 00:34:25 +0900

    Title: 알파벳에 따른 평가, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+c=input()
+
+if c=='S':
+    print("Superior")
+elif c=='A':
+    print("Excellent")
+elif c=='B':
+    print("Good")
+elif c=='C':
+    print("Usually")
+elif c=='D':
+    print("Effort")
+else:
+    print("Failure")
