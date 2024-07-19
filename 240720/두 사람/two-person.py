14fc0b0c36428aa22d3cce63067c5808d7f28296
winA <92146550+winnaba61@users.noreply.github.com>
2024-07-20 01:17:45 +0900

    Title: 두 사람, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240720/두 사람/two-person.py
+++ 240720/두 사람/two-person.py
@@ -1,5 +1,5 @@
-age_a,sex_a=map(input().split())
-age_b,sex_b=map(input().split())
+age_a,sex_a=map(str,input().split())
+age_b,sex_b=map(str,input().split())
 
 if (int(age_a)>=19 or sex_a=='M') or (int(age_b)>=19 or sex_b=='M'):
     print(1)
