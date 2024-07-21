61ce1279b6a16acd65f658d5575cdfb489df3e97
winA <92146550+winnaba61@users.noreply.github.com>
2024-07-21 10:30:49 +0900

    Title: 별표 출력하기 13, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240721/별표 출력하기 13/print-start-13.py
+++ 240721/별표 출력하기 13/print-start-13.py
@@ -1,10 +1,11 @@
 def print_stars(n):
     print('* ' * n)
     
-    for i in range(1, n):
-        print('* ' * i)
-    for i in range(n-1, 0, -1):
-        print('* ' * i)
+    for i in range(n-1):
+        if i % 2 == 0:
+            print('*')
+        else:
+            print('* ' * (n - 1))
     
     print('* ' * n)
 
