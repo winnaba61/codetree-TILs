bf80885982593857f3587ab986a5236280ac22aa
winA <92146550+winnaba61@users.noreply.github.com>
2024-07-22 13:43:24 +0900

    Title: 짝수만 거꾸로 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240722/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
+++ 240722/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.py
@@ -3,9 +3,11 @@ ans = []
 
 for i in range(n):
     nums=list(map(int,input().split()))
-    for num in nums:
-        if num%2==0:
-            ans.append(num)
+
+
+for num in nums:
+    if num%2==0:
+        ans.append(num)
 
 while ans:
     print(ans.pop())
